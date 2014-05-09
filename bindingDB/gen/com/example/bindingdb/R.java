7d1412fb1c56a6e03e48d670d9b4fb0d20fc86ed
jiashenwang <wangjiashenwy@gmail.com>
2014-05-09 09:40:37 -0700

    doing search page 1

--- bindingDB/gen/com/example/bindingdb/R.java
+++ bindingDB/gen/com/example/bindingdb/R.java
@@ -8,6 +8,10 @@
 package com.example.bindingdb;
 
 public final class R {
+    public static final class array {
+        public static final int descriptions=0x7f060001;
+        public static final int titles=0x7f060000;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -21,16 +25,22 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int caf=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f090003;
+        public static final int image=0x7f090002;
+        public static final int result_list=0x7f090001;
+        public static final int temp=0x7f090000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int search_result=0x7f030001;
+        public static final int single_row=0x7f030002;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
@@ -59,10 +69,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
--- bindingDB/src/com/example/bindingdb/MainActivity.java
+++ bindingDB/src/com/example/bindingdb/MainActivity.java
@@ -3,20 +3,20 @@ package com.example.bindingdb;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
+	
+	Button button;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		 button = (Button)findViewById(R.id.temp);
+		 
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
 
 }
