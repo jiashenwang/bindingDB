2e1edc8a49684d92ebe0235f1d89f643a97487cf
tngyn <timngyn@gmail.com>
2014-05-09 03:58:06 -0700

    Main activity screen layouts

--- bindingDB/gen/com/example/bindingdb/R.java
+++ bindingDB/gen/com/example/bindingdb/R.java
@@ -24,7 +24,12 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080005;
+        public static final int compound_search_header=0x7f080004;
+        public static final int protein_name_search=0x7f080001;
+        public static final int protein_search_btn=0x7f080003;
+        public static final int protein_search_header=0x7f080000;
+        public static final int uniprot_search=0x7f080002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -35,7 +40,11 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int protein_name_hint=0x7f050003;
+        public static final int protein_search_btn=0x7f050006;
+        public static final int search_compound=0x7f050005;
+        public static final int search_protein=0x7f050002;
+        public static final int uniprot_hint=0x7f050004;
     }
     public static final class style {
         /** 
